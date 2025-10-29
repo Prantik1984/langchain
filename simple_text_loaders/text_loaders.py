9a203c72485a5b9a9b85dc52fcc3aac8b0289678
Prantik <prantik.sarmah84@gmail.com>
2025-10-29 20:02:17 +0530

    cleaning text

--- simple_text_loaders/text_loaders.py
+++ simple_text_loaders/text_loaders.py
@@ -1,11 +1,19 @@
 from langchain_community.document_loaders import TextLoader as LangLoader
+from dotenv import load_dotenv
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import re
+import  os
 class TextLoader:
   def __init__(self):
+    load_dotenv()
     documents = LangLoader("./data/dream.txt").load()
     cleaned_documents=[self.clean_text(doc.page_content) for doc in documents]
-    print(cleaned_documents)
-  
+    chunk_size=int(os.getenv("CHUNK_SIZE"))
+    chunk_overlap=int(os.getenv("chunk_overlap"))
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size, chunk_overlap)
+    # texts = text_splitter.split_documents(documents)
+    # print(texts)
+
   def clean_text(self,txt):
     txt = re.sub(r"[^a-zA-Z\s]", "", txt)
     txt = re.sub(r"\s+", " ", txt).strip()
