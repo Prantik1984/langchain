136211626bc8c146a88ab8dee25332cc05ce9e84
Prantik <prantik.sarmah84@gmail.com>
2025-10-29 19:46:27 +0530

    cleaning text

--- simple_text_loaders/text_loaders.py
+++ simple_text_loaders/text_loaders.py
@@ -1,9 +1,16 @@
 from langchain_community.document_loaders import TextLoader as LangLoader
+import re
 class TextLoader:
   def __init__(self):
     documents = LangLoader("./data/dream.txt").load()
-    print(documents)
+    cleaned_documents=[self.clean_text(doc.page_content) for doc in documents]
+    print(cleaned_documents)
   
+  def clean_text(self,txt):
+    txt = re.sub(r"[^a-zA-Z\s]", "", txt)
+    txt = re.sub(r"\s+", " ", txt).strip()
+    txt=txt.lower()
+    return txt
 
 if __name__ == "__main__":
     loader = TextLoader()
