a204535e1bb3e0a3adfce5323bc9d70a38b1d79c
Prantik <prantik.sarmah84@gmail.com>
2025-11-17 19:46:25 +0530

    updating

--- simple_text_loaders/EmbedingsCreator.py
+++ simple_text_loaders/EmbedingsCreator.py
@@ -1,5 +1,6 @@
 from langchain_ollama import OllamaEmbeddings
 from dotenv import load_dotenv
+from langchain_community.vectorstores import FAISS
 import  os
 
 class EmbedingsCreator:
@@ -7,11 +8,12 @@ class EmbedingsCreator:
        load_dotenv()
        self.Ollama_Url=os.getenv("ollama_url")
        self.embedding_model = os.getenv("embedding_model")
+       self.persist_path = os.getenv("persist_path")
 
-    def create_embedings(self,text):
+    def create_embedings(self,texts):
         embeddings = OllamaEmbeddings(
             model=self.embedding_model,
             base_url=self.Ollama_Url
         )
-        vector = embeddings.embed_query(text)
-        return embeddings
+
+        return FAISS.from_texts(texts, embeddings)
--- simple_text_loaders/text_loaders.py
+++ simple_text_loaders/text_loaders.py
@@ -13,22 +13,21 @@ class TextLoader:
   def __init__(self):
     load_dotenv()
     documents = LangLoader("./data/dream.txt").load()
-    cleaned_documents=[self.clean_text(doc.page_content) for doc in documents]
     chunk_size=int(os.getenv("CHUNK_SIZE"))
     chunk_overlap=int(os.getenv("chunk_overlap"))
     text_splitter = RecursiveCharacterTextSplitter(
       chunk_size=chunk_size,
       chunk_overlap=chunk_overlap
     )
+
     texts = text_splitter.split_documents(documents)
     texts=[self.clean_text(text.page_content) for text in texts]
     embedder = EmbedingsCreator()
-    for text in texts:
-      embedings = embedder.create_embedings(text)
-
-    retriever = FAISS.from_texts(texts,embedings).as_retriever(search_kwargs={"k": 2})
+    embeddings=embedder.create_embedings(texts)
+    retriever=embeddings.as_retriever(search_kwargs={"k": 2})
 
     query = "Give me a summary of the speech in bullet points"
+    query = "What does the speaker say about freedon"
     docs = retriever.invoke(query)
 
     prompt = ChatPromptTemplate.from_template(
